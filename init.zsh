adf59d2623119d92f679853e07c28431a92eafcb
Song Jeyeong <46372718+hooreique@users.noreply.github.com>
2025-02-23 20:41:53 +0900

    misc

--- init.zsh
+++ init.zsh
@@ -5,7 +5,7 @@ elif [[ -f /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh ]]; then
   source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 fi
 
-# Generate random key for SSHD
+# Generate random key for sshd
 if [[ ! -f ~/.ssh/host_ed25519 ]]; then
   ssh-keygen -t ed25519 -f ~/.ssh/host_ed25519 -N ""
 fi
