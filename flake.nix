b847a5e22e27f1cf6c117e658bc6e8782de8a836
Song Jeyeong <46372718+hooreique@users.noreply.github.com>
2025-04-26 11:18:02 +0900

    misc

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Home Manager configuration of song";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     home-manager = {
       url = "github:nix-community/home-manager";
