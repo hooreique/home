bc511dbca38420e16bdd8690894e3bef8d13a702
Song Jeyeong <46372718+hooreique@users.noreply.github.com>
2025-08-23 07:07:35 +0900

    add inetutils

--- home.nix
+++ home.nix
@@ -17,7 +17,7 @@
     # Dogfooding
     fall  lepo
 
-    bash  openssh  lsof  less  gnused  perl  jq  curl  elinks  entr
+    bash  openssh  lsof  less  gnused  perl  jq  curl  elinks  entr  inetutils
     ncurses # clear, infocmp, tic
     uutils-coreutils-noprefix # cat, cp, mkdir ...
     zellij  eza
