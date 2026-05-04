20784e39e63db6e2bb0c7fb12d94084f89713031
Song Jeyeong <46372718+hooreique@users.noreply.github.com>
2026-05-05 05:39:57 +0900

    darwin: keyrepeat

--- home-darwin.nix
+++ home-darwin.nix
@@ -1,28 +1,16 @@
 { pkgs, lib, ... }:
 
 lib.mkIf pkgs.stdenv.isDarwin {
-  targets.darwin.defaults.NSGlobalDomain.NSWindowShouldDragOnGesture = true;
-
-  targets.darwin.defaults."com.apple.symbolichotkeys".AppleSymbolicHotKeys = {
-    # Keyboard Shortcuts > Input Sources > Select the previous input source: F19
-    "60" = {
-      enabled = true;
-      value.type = "standard";
-      value.parameters = [
-        65535
-        80
-        8388608
-      ];
-    };
-    # Keyboard Shortcuts > Input Sources > Select next source in Input menu: F18
-    "61" = {
-      enabled = true;
-      value.type = "standard";
-      value.parameters = [
-        65535
-        79
-        8388608
-      ];
+  targets.darwin.defaults = {
+    NSGlobalDomain.NSWindowShouldDragOnGesture = true;
+    NSGlobalDomain.KeyRepeat = 2; # 2..120 (lower is faster)
+    NSGlobalDomain.InitialKeyRepeat = 15; # 15..120 (lower is shorter)
+    "com.apple.HIToolbox".AppleFnUsageType = 2; # Show Emoji & Symbols
+    "com.apple.symbolichotkeys".AppleSymbolicHotKeys = {
+      # Keyboard Shortcuts > Input Sources > Select the previous input source: F19
+      "60" = { enabled = true; value.type = "standard"; value.parameters = [ 65535 80 8388608 ]; };
+      # Keyboard Shortcuts > Input Sources > Select next source in Input menu: F18
+      "61" = { enabled = true; value.type = "standard"; value.parameters = [ 65535 79 8388608 ]; };
     };
   };
 }
