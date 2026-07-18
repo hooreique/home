43a0c57e81cd49df5ab9eb9929941d98d7a437ee
Song Jeyeong <46372718+hooreique@users.noreply.github.com>
2026-07-19 07:46:52 +0900

    fix(zsh): avoid reloading Nix daemon profile

--- home.nix
+++ home.nix
@@ -74,7 +74,8 @@
       }
     ];
     envExtra = ''
-      if [[ -f /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh ]]; then
+      if [[ -z "''${NIX_PROFILES-}" \
+        &&  -f /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh ]]; then
         source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
       fi
 
