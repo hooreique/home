e2af6ed59da307d490b7a52e3a4551f7bc03cf4e
Song Jeyeong <46372718+hooreique@users.noreply.github.com>
2026-07-29 01:13:44 +0900

    nixos-gnome patch

--- flake.nix
+++ flake.nix
@@ -7,10 +7,16 @@
     fall.url         = "github:hooreique/fall";
     hvim.url         = "github:hooreique/hvim";
     saseo.url        = "github:hooreique/saseo";
+
+    # For nixos-gnome
+    nixpkgs-de.url = "https://flakehub.com/f/NixOS/nixpkgs/0";
+    nur.url        = "github:nix-community/NUR";
+    soop.url       = "github:hooreique/soop";
+    spoofdpium.url = "github:hooreique/spoofdpium";
   };
 
   outputs = inputs: let
-    system = "aarch64-linux";
+    system = "x86_64-linux";
     my-pkgs.fall  = inputs.fall.packages.${system}.default;
     my-pkgs.hvim  = inputs.hvim.packages.${system}.default;
     my-pkgs.saseo = inputs.saseo.packages.${system}.default;
@@ -23,6 +29,15 @@
       };
       extraSpecialArgs = {
         inherit my-pkgs;
+        de-pkgs = import inputs.nixpkgs-de {
+          inherit system;
+          config.allowUnfree = true;
+          overlays = [
+            inputs.nur.overlays.default
+            inputs.soop.overlays.default
+            inputs.spoofdpium.overlays.default
+          ];
+        };
       };
       modules = [
         {
@@ -30,6 +45,7 @@
           home.homeDirectory = "/home/song";
         }
         ./home.nix
+        ./home-nixos-gnome.nix
       ];
     };
   };
