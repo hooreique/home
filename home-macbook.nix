7d199c369506aa0549df23ae0cdb8b394ea09a79
Song Jeyeong <46372718+hooreique@users.noreply.github.com>
2026-08-22 15:18:47 +0900

    macbook patch

---
+++ home-macbook.nix
@@ -0,0 +1,14 @@
+{ pkgs, ... }:
+
+{
+  home.packages = [ pkgs.macism ];
+
+  programs.hisle.enable = true;
+
+  targets.darwin.defaults = {
+    NSGlobalDomain.NSWindowShouldDragOnGesture = true;
+    NSGlobalDomain.KeyRepeat = 2; # 2..120 (lower is faster)
+    NSGlobalDomain.InitialKeyRepeat = 15; # 15..120 (lower is shorter)
+    "com.apple.HIToolbox".AppleFnUsageType = 2; # Show Emoji & Symbols
+  };
+}
