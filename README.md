8644e0911d19f2b387b8f14829e966e391eec000
Song Jeyeong <46372718+hooreique@users.noreply.github.com>
2026-08-30 21:56:45 +0900

    zellij: migrate kdl file -> home manager option

--- README.md
+++ README.md
@@ -10,5 +10,4 @@ nix run nixpkgs#home-manager -- switch
 
 - `~/.ssh/id_ed25519` is used for SSH and Git commit signing.
 - `~/init.zsh` is sourced when present; keep host-local hacks there.
-- Zellij config switches by platform: `zellij.mac.kdl` on Darwin, `zellij.win.kdl` elsewhere.
 - On WSL, keep `/etc/wsl.conf` with `appendWindowsPath=false`. See https://learn.microsoft.com/en-us/windows/wsl/wsl-config#interop-settings
--- home.nix
+++ home.nix
@@ -7,7 +7,7 @@
     # Dogfooding
     my-pkgs.fall  my-pkgs.hvim  my-pkgs.saseo
 
-    bash  man  lsof  less  gnused  perl  jq  entr
+    bash  man  less  gnused  perl  jq
     openssh  openssl  curl
     dig                       # nslookup
     inetutils                 # telnet
@@ -17,11 +17,11 @@
     uutils-coreutils-noprefix # cat, cp, mkdir
     uutils-findutils          # find
     unixtools.watch
-    eza  zellij
+    entr  lsof
     vim  neovim
     fd  ripgrep
     delta  difftastic
-    nushell
+    eza  nushell
   ];
 
   home.sessionVariables = {
@@ -35,21 +35,121 @@
 
   home.file.".hushlogin".text = "";
 
-  xdg.configFile."zellij/config.kdl".source = ./zellij.${
-    if pkgs.stdenv.hostPlatform.isDarwin then "mac" else "win"
-  }.kdl;
-  xdg.configFile."zellij/layouts/compact.kdl".text = ''
-    layout {
-      pane
-      pane size=1 borderless=true {
-        plugin location="compact-bar"
-      }
-    }
-  '';
-
   xdg.enable = true;
 
   programs.home-manager.enable = true;
+
+  programs.zellij = let
+    mod = if pkgs.stdenv.hostPlatform.isDarwin then "Super" else "Alt";
+  in {
+    enable = true;
+
+    settings = {
+      session_serialization = false;
+      show_startup_tips = false;
+      pane_frames = false;
+      simplified_ui = true;
+      default_layout = "compact";
+      theme = "sonokai";
+
+      web_client.theme = {
+        foreground           = [ 225 227 228 ];
+        background           = [  42  47  56 ];
+        cursor               = [ 225 227 228 ];
+        cursor_accent        = [  42  47  56 ];
+        selection_background = [  61  68  85 ];
+        black                = [  42  47  56 ];
+        red                  = [ 255 101 120 ];
+        green                = [ 157 210 116 ];
+        yellow               = [ 234 203 100 ];
+        blue                 = [ 246 156  94 ];
+        magenta              = [ 186 156 243 ];
+        cyan                 = [ 114 204 232 ];
+        white                = [ 225 227 228 ];
+        bright_black         = [ 130 138 154 ];
+        bright_red           = [ 255 101 120 ];
+        bright_green         = [ 157 210 116 ];
+        bright_yellow        = [ 234 203 100 ];
+        bright_blue          = [ 246 156  94 ];
+        bright_magenta       = [ 186 156 243 ];
+        bright_cyan          = [ 114 204 232 ];
+        bright_white         = [ 225 227 228 ];
+      };
+    };
+
+    themes.sonokai.themes.sonokai = {
+      bg      = "#424b5b";
+      black   = "#333846";
+      blue    = "#72cce8";
+      cyan    = "#7ad5f1";
+      fg      = "#5a6477";
+      green   = "#a5e179";
+      magenta = "#ba9cf3";
+      orange  = "#f69c5e";
+      red     = "#ff6d7e";
+      white   = "#828a9a";
+      yellow  = "#eacb64";
+    };
+
+    layouts.compact = ''
+      layout {
+        pane
+        pane size=1 borderless=true {
+          plugin location="compact-bar"
+        }
+      }
+    '';
+
+    extraConfig = ''
+      keybinds clear-defaults=true {
+        normal {
+          bind "${mod} d" { Detach; }
+
+          bind "${mod} a" { SwitchToMode "resize"; }
+
+          bind "${mod} k" { Clear; }
+
+          bind "${mod} o" {
+            LaunchOrFocusPlugin "session-manager" {
+              floating true
+              move_to_focused_tab true
+            };
+          }
+
+          bind "${mod} p" { NewTab; }
+          bind "${mod} w" { CloseTab; }
+          bind "${mod} 1" { GoToTab 1; }
+          bind "${mod} 2" { GoToTab 2; }
+          bind "${mod} 3" { GoToTab 3; }
+          bind "${mod} 4" { GoToTab 4; }
+          bind "${mod} 5" { GoToTab 5; }
+          bind "${mod} 6" { GoToTab 6; }
+          bind "${mod} 7" { GoToTab 7; }
+          bind "${mod} 8" { GoToTab 8; }
+          bind "${mod} 9" { GoToTab 9; }
+          bind "${mod} l" { GoToPreviousTab; }
+          bind "${mod} y" { GoToNextTab; }
+
+          bind "${mod} s" { NewPane "Down"; }
+          bind "${mod} t" { NewPane "Right"; }
+          bind "${mod} u" { MoveFocus "Up"; }
+          bind "${mod} e" { MoveFocus "Down"; }
+          bind "${mod} n" { MoveFocus "Left"; }
+          bind "${mod} i" { MoveFocus "Right"; }
+        }
+
+        resize {
+          bind "Esc" { SwitchToMode "normal"; }
+          bind "q"   { SwitchToMode "normal"; }
+
+          bind "u" { Resize "Increase Up"; }
+          bind "e" { Resize "Increase Down"; }
+          bind "n" { Resize "Increase Left"; }
+          bind "i" { Resize "Increase Right"; }
+        }
+      }
+    '';
+  };
 
   programs.ssh = {
     enable = true;
